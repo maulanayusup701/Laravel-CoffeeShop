8265cbbc4cd64af6e3c5f933a15dd00e94af6e70
Maulana Yusup <maulana.yusup8989@gmail.com>
2023-08-19 15:53:29 +0700

    auth

---
+++ resources/views/layouts/components/script.blade.php
@@ -0,0 +1,6 @@
+<script src="{{ asset('back-end/assets/js/bootstrap.js') }}"></script>
+<script src="{{ asset('back-end/assets/js/app.js') }}"></script>
+
+<script src="{{ asset('back-end/assets/js/pages/dashboard.js') }}"></script>
+
+</html>
